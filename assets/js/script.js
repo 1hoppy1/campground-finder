bdbb042c1f243703a4d01fd844fad237c9b4594f
1hoppy1 <jahopki@hotmail.com>
2020-06-11 09:15:48 -0600

    basic html,css,js setup of project

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//This makes the Side Bar work
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+/*This notation section only needed if we use side navigation bar and want collapsible option
+//Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+var collapsibleElem = document.querySelector('.collapsible');
+var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+*/
+
+//This makes the state,province,park dropdown boxes work
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+
+
+//This makes the Modal work
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
