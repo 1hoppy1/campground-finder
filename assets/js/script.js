02c9dae9bce81d75a3934f5d46f02466f5213f9c
CRSPN <unconfigured@null.spigotmc.org>
2020-06-11 19:24:42 -0600

    began adding keys to APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 // add key to open weather
 
+ $.ajax({
+     url:"http://api.amp.active.com/camping/campground/details?contractCode=CO&parkId=50032&api_key=ajx3dgbbz3xz9ftbez8be438",
+     method:"GET"
+    
+ }).then (function(campgroundResults){
+     console.log("campground-api",campgroundResults)
+     $.ajax({
+        url:"https://api.openweathermap.org/data/2.5/weather?lat=38.8947222&lon=-105.1794444&units=imperial&appid="+keys.OPENWEATHER,
+        method:"GET"
+    }).then (function(results){
+        console.log("openweather",results)
+    })
+ })
+
+
  
