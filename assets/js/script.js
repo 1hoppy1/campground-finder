43d7ff1d9b665d39445bad04e9f5c1864e6c2d84
Chris Hadley <hadleyc15@yahoo.com>
2020-06-13 14:26:27 -0600

    added js to ping api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
 var stateSelect = document.getElementById("state-dropdown-menu");
 var provinceSelect = document.getElementById("province-dropdown-menu");
+var apiKey = "&api_key=afvahcy3rpqnc76atjxg4kjk";
+var queryValue;
+//add function to grab campground api here...
 
-stateSelect.addEventListener('change', function(){console.log('change')})
-provinceSelect.addEventListener('change', function(){console.log('change')})
+var queryURL = "https://api.amp.active.com/camping/campgrounds/?pstate=" 
+
+//add function to grab weather data here....
+function getCampgroundData (area) {
+    $.ajax({
+        type: "GET",
+        url: queryURL + area + apiKey
+    })
+    .then (function(data){
+        console.log(data)
+    })
+}
+
+stateSelect.addEventListener('change', function () {
+     console.log('change') 
+     queryValue = stateSelect.options[stateSelect.selectedIndex].value 
+     console.log(queryValue)
+    getCampgroundData(queryValue);
+    })
+
+
+
+
+provinceSelect.addEventListener('change', function () { 
+    console.log('change') 
+
+})
 
 
 
