078ee4b32044016319a3e943c4dd397a77759110
Chris Hadley <hadleyc15@yahoo.com>
2020-06-14 14:28:17 -0600

    added updates to js to console log data from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,83 @@
 var stateSelect = document.getElementById("state-dropdown-menu");
-var apiKey = "&api_key=8PYNKmCCNOZNgeQnteDYJtKAQXejR1odSgowgYcn";
+var provinceSelect = document.getElementById("province-dropdown-menu");
+var apiKey = "&api_key=afvahcy3rpqnc76atjxg4kjk";
 var queryValue;
 //add function to grab campground api here...
 
-var queryURL = "https://developer.nps.gov/api/v1/campgrounds?stateCode=" 
+var queryURL = "https://cors-anywhere.herokuapp.com/http://api.amp.active.com/camping/campgrounds/?pstate="
 
 //add function to grab weather data here....
-function getCampgroundData (area) {
+function getCampgroundData(area) {
     $.ajax({
         type: "GET",
         url: queryURL + area + apiKey
     })
-    .then (function(data){
-        console.log(data)
-    })
+        .then(function (data) {
+            console.log(data)
+            var dataAsJSON = xmlToJson(data);
+            
+            var resultData = dataAsJSON.resultset.result
+        for (var i = 0; i < resultData.length; i++) {
+            console.log(resultData[i]['@attributes'])
+        }
+})}
+
+jQuery.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
+function xmlToJson(xml) {
+
+    // Create the return object
+    var obj = {};
+
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) { // text
+        obj = xml.nodeValue;
+    }
+
+    // do children
+    if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof (obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof (obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
 }
 
-stateSelect.addEventListener('change', function () {
-     console.log('change') 
-     queryValue = stateSelect.options[stateSelect.selectedIndex].value 
-     console.log(queryValue)
-    getCampgroundData(queryValue);
+    stateSelect.addEventListener('change', function () {
+        queryValue = stateSelect.options[stateSelect.selectedIndex].value
+        console.log(queryValue)
+        getCampgroundData(queryValue);
     })
 
-//"https://developer.nps.gov/api/v1/campgrounds?stateCode=UT&api_key=8PYNKmCCNOZNgeQnteDYJtKAQXejR1odSgowgYcn"
+
+
+
+    provinceSelect.addEventListener('change', function () {
+        queryValue = provinceSelect.options[provinceSelect.selectedIndex].value
+        console.log(queryValue)
+        getCampgroundData(queryValue);
+
+    })
